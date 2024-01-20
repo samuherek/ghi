d35c269034d31698ffea6d2b04611d2e1cab3960
Sam Uherek <samuherekbiz@gmail.com>
2024-01-20 16:27:17 +0100

    implement basic lexer for unix commands

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,12 +1,14 @@
 
 #[derive(Debug, PartialEq)]
 enum Token {
-    LSquare,
-    RSquare,
-    LArrow, 
-    RArrow,
+    LSq,
+    RSq,
+    LAr, 
+    RAr,
+    Flag,
+    Str(String),
     Illegal,
-    Eof
+    Eof,
 }
 
 struct CmdLexer<'a > {
@@ -22,7 +24,7 @@ impl<'a> CmdLexer<'a> {
             input,
             position: 0,
             read_position: 0,
-            ch: '\n'
+            ch: '0'
         };
         lexer.read_char();
         return lexer;
@@ -30,7 +32,7 @@ impl<'a> CmdLexer<'a> {
 
     fn read_char(&mut self) {
         if self.read_position >= self.input.len() {
-            self.ch = '\n';
+            self.ch = '0';
         } else {
             self.ch = self.input
                 .chars()
@@ -42,13 +44,45 @@ impl<'a> CmdLexer<'a> {
         self.read_position += 1;
     }
 
+    fn read_str(&mut self) -> String {
+        let pos = self.position;
+        while is_str_letter(self.ch) {
+            self.read_char();
+        }
+        return self.input[pos..self.position].to_string();
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == ' ' {
+            self.read_char();
+        }
+    }
+
     fn next_token(&mut self) -> Token {
-        let token =match self.ch {
-            '[' => Token::LSquare,
+        self.skip_whitespace();        
+
+        let token = match self.ch {
+            '[' => Token::LSq,
+            ']' => Token::RSq,
+            '<' => Token::LAr,
+            '>' => Token::RAr,
+            '-' => Token::Flag,
+            'a'..='z' | 'A'..='Z' => {
+                let value = self.read_str(); 
+                return Token::Str(value);
+            },
+            '0' => Token::Eof,
             _ => Token::Illegal
         };
         self.read_char();
         return token;
+    }
+}
+
+fn is_str_letter(input: char) -> bool {
+    match input {
+        'a'..='z' | 'A'..='Z' | '-' | '_' => true,
+        _ => false
     }
 }
 
@@ -61,14 +95,19 @@ fn split_cmd(input: &str) -> (&str, &str) {
 }
 
 //"suspend-client [-t <target-client>]"
-pub fn lex(input: &str) -> Vec<String> {
+pub fn lex(input: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
-    let mut lexer = CmdLexer::new(input);
-    lexer.next_token(); 
+    let mut lexer = CmdLexer::new(&input);
+    let mut to_next = true;
 
-    println!("{}", {input});
+    while to_next {
+        let token = lexer.next_token(); 
+        if token == Token::Eof {
+            to_next = false;
+        }
+        tokens.push(token);
+    }
 
-    tokens.push("hellow".to_string());
     return tokens;
 }
 
@@ -90,15 +129,70 @@ mod tests {
 
     #[test]
     fn next_token() {
-        let value = "choose-client [-t <target-session>]";
+        let value = "choose-client []";
         let (_, input) = super::split_cmd(&value);
         let mut lexer = super::CmdLexer::new(&input);
 
         let token = lexer.next_token();
-        assert_eq!(token, super::Token::LSquare);
+        assert_eq!(token, super::Token::LSq);
 
         let token = lexer.next_token();
-        assert_eq!(token, super::Token::LSquare);
+        assert_eq!(token, super::Token::RSq);
 
+    }
+    
+    #[test]
+    fn lex() {
+        let input = "[]<>f";
+        let exp = vec![
+            super::Token::LSq,
+            super::Token::RSq,
+            super::Token::LAr,
+            super::Token::RAr,
+            super::Token::Str(String::from("f")),
+            super::Token::Eof,
+        ];
+
+        let result = super::lex(&input);
+
+        assert_eq!(result, exp);
+    }
+
+    #[test]
+    fn flag() {
+        let input = "[]<>-fa[";
+        let exp = vec![
+            super::Token::LSq,
+            super::Token::RSq,
+            super::Token::LAr,
+            super::Token::RAr,
+            super::Token::Flag,
+            super::Token::Str(String::from("fa")),
+            super::Token::LSq,
+            super::Token::Eof,
+        ];
+
+        let result = super::lex(&input);
+
+        assert_eq!(result, exp);
+    }
+
+    #[test]
+    fn command() {
+        let input = "[-t <target-session>]";
+        let exp = vec![
+            super::Token::LSq,
+            super::Token::Flag,
+            super::Token::Str(String::from("t")),
+            super::Token::LAr,
+            super::Token::Str(String::from("target-session")),
+            super::Token::RAr,
+            super::Token::RSq,
+            super::Token::Eof,
+        ];
+
+        let result = super::lex(&input);
+
+        assert_eq!(result, exp);
     }
 }
