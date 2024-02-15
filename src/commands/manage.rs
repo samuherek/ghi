10c4141d59e548ba69e7e5944998e894aa22f5de
Sam Uherek <samuherekbiz@gmail.com>
2024-02-15 20:50:49 +0100

    render simple explorere for lesssons and quets

--- src/commands/manage.rs
+++ src/commands/manage.rs
@@ -1,5 +1,5 @@
 use diesel::SqliteConnection;
-use crossterm::{execute, terminal, QueueableCommand};
+use crossterm::{execute, terminal, QueueableCommand, cursor};
 use crossterm::event::{poll, read, Event, KeyCode, KeyEventKind, KeyModifiers};
 use crossterm::style::Color;
 use crate::screen;
@@ -15,10 +15,13 @@ fn query_lessons(conn: &mut SqliteConnection) -> Vec<models::Lesson> {
 }
 
 enum View {
-    Lessons
+    Lessons,
+    Quests
 }
 
 struct State {
+    lessons_idx: usize,
+    quests_idx: usize,
     lessons: Vec<models::Lesson>,
     quests: Vec<models::Quest>,
     view: View
@@ -29,23 +32,87 @@ impl State {
         let lessons = query_lessons(conn);
 
         Self {
+            lessons_idx: 0,
+            quests_idx: 0,
             lessons,
             quests: Vec::new(),
             view: View::Lessons
         }
     }
 
-    // fn render_header(&self, page: &mut Screen) {
-    //     let tl = page.rect.top_left_padded().add(0, 15);
-    //     let text = "You missed it:".chars().map(|ch| screen::Cell::new(ch, Color::White)).collect();
-    //     page.next_buf.put_cells(tl, text);
-    // }
+    fn select_up(&mut self) {
+        match self.view {
+            View::Lessons => self.lessons_idx = self.lessons_idx.saturating_sub(1),
+            View::Quests => self.quests_idx = self.quests_idx.saturating_sub(1),
+        }
+    }
 
-    fn render_lessons(&self, page: &mut Screen) {
-        let tl = page.rect.top_left_padded();
+    fn select_down(&mut self) {
+        match self.view {
+            View::Lessons => {
+                let max = self.lessons.len() - 1;
+                let max_len = if self.lessons_idx >= max {
+                    max
+                } else {
+                    self.lessons_idx + 1
+                };
+                self.lessons_idx = max_len;
+            },
+            View::Quests => {
+                let max = self.quests.len();
+                let max_len = if self.quests_idx >= max {
+                    max
+                } else {
+                    self.quests_idx + 1
+                };
+                self.quests_idx = max_len;
+            }
+        }
+    }
+
+    fn select(&mut self) {
+        match self.view {
+            View::Lessons => self.view = View::Quests,
+            View::Quests => self.view = View::Lessons,
+        }
+    }
+
+    fn render_header(&self, page: &mut Screen, point: screen::Point) {
+        let text = "Your available courses".chars().map(|ch| screen::Cell::new(ch, Color::White)).collect();
+        page.next_buf.put_cells(point, text);
+    }
+
+    fn render_divider(&self, page: &mut Screen, point: screen::Point) {
+        let cells = vec![screen::Cell::new('-', Color::White); page.rect.width().into()];
+        page.next_buf.put_cells(point, cells);
+    }
+
+    fn render_back(&self, page: &mut Screen, point: screen::Point) {
+        let cells = "..".chars().map(|ch| screen::Cell::new(ch, Color::White)).collect();
+        page.next_buf.put_cells(point.add(3, 0), cells);
+    }
+
+    fn render_selection_caret(&self, page: &mut Screen, point: screen::Point) {
+        let idx = match self.view {
+            View::Lessons => self.lessons_idx,
+            View::Quests => self.quests_idx,
+        };
+        let pnt = point.add(0, idx as u16);
+        page.next_buf.put_cell(pnt, screen::Cell::new('>', Color::White));
+    }
+
+    fn render_lessons(&self, page: &mut Screen, point: screen::Point) {
         for (offset, lesson) in self.lessons.iter().enumerate() {
-            let point = tl.add(0, offset as u16);
+            let point = point.add(3, offset as u16);
             let cells = lesson.cmd.chars().map(|ch| screen::Cell::new(ch, Color::White)).collect();
+            page.next_buf.put_cells(point, cells);
+        }
+    }
+
+    fn render_quests(&self, page: &mut Screen, point: screen::Point) {
+        for (offset, quest) in self.quests.iter().enumerate() {
+            let point = point.add(3, offset as u16);
+            let cells = quest.cmd_name.chars().map(|ch| screen::Cell::new(ch, Color::White)).collect();
             page.next_buf.put_cells(point, cells);
         }
     }
@@ -53,11 +120,23 @@ impl State {
     fn render(&self, page: &mut Screen) {
         match self.view {
             View::Lessons => {
-                // self.render_header(page);
-                self.render_lessons(page);
+                let pnt = page.rect.top_left_padded();
+                self.render_header(page, pnt.add(0, 0));
+                self.render_divider(page, pnt.add(0, 3));
+                self.render_lessons(page, pnt.add(0, 4));
+                self.render_selection_caret(page, pnt.add(0, 4)); 
+                let _ = page.stdout.queue(cursor::Hide);
             },
+            View::Quests => {
+                let pnt = page.rect.top_left_padded();
+                self.render_header(page, pnt.add(0, 0));
+                self.render_divider(page, pnt.add(0, 3));
+                self.render_back(page, pnt.add(0, 4));
+                self.render_quests(page, pnt.add(0, 5));
+                self.render_selection_caret(page, pnt.add(0, 4)); 
+                let _ = page.stdout.queue(cursor::Hide);
+            }
         }
-
     }
 }
 
@@ -154,11 +233,20 @@ pub fn run(conn: &mut SqliteConnection) -> std::io::Result<()> {
                 },
                 Event::Key(event) if event.kind == KeyEventKind::Press => {
                     match event.code {
-                        KeyCode::Char(x) => {
-                            if event.modifiers.contains(KeyModifiers::CONTROL) && x == 'c' {
+                        KeyCode::Char(ch) => {
+                            if event.modifiers.contains(KeyModifiers::CONTROL) && ch == 'c' {
                                 page.quit()
+                            } else {
+                                match ch {
+                                    'j' => state.select_down(),
+                                    'k' => state.select_up(),
+                                    _ =>  {}
+                                }
                             }
                         },
+                        KeyCode::Up => state.select_up(),
+                        KeyCode::Down => state.select_down(),
+                        KeyCode::Enter => state.select(),
                         _ => {}
                     }
                 },
