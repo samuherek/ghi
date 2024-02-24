65f8e9b441db5626f06ae5b6a7d9421479197644
Sam Uherek <samuherekbiz@gmail.com>
2024-02-24 21:33:06 +0100

    base setup for a config file

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod db;
 use clap::{Parser, Subcommand};
 use simplelog::{WriteLogger, LevelFilter, Config};
 use log::info;
+use dirs;
+use std::path::PathBuf;
+use serde::Deserialize;
 
 #[derive(Parser)]
 #[command(author = "Sam Uherek <samuherekbiz@gmail.com>")]
@@ -28,9 +31,50 @@ pub enum Commands {
     Run
 }
 
+struct GhiConfig {
+    // - $HOME/.ghi
+    config_dir: PathBuf,
+    // - $HOME/.ghirc
+    // - $HOME/.ghi/config
+    // - $HOME/.config/ghi/config
+    database_path: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct GhiUserConfig {
+   database_path: Option<String>,
+}
+
+const CONFIG_PATHS: [&str; 2] = [".ghi/config.toml", ".config/ghi/config.toml"];
+
+fn get_config() -> GhiConfig {
+    let home = dirs::home_dir().expect("Could not resolve home directory");
+    let first_config_path = CONFIG_PATHS.iter()
+        .map(|path| home.join(path))
+        .find(|path| path.exists());
+
+    let config = if let Some(first_config_path) = first_config_path {
+        let data = std::fs::read_to_string(first_config_path).expect("The config path to exist");
+        let user_config: GhiUserConfig = toml::from_str(&data).expect("Toml config could not parse");
+        GhiConfig {
+            config_dir: home.join(".ghi"),
+            database_path: user_config.database_path
+        }
+    } else {
+        GhiConfig {
+            config_dir: home.join(".ghi"),
+            database_path: Some(home.join(".ghi/database.sql").display().to_string())
+        }
+    };
+
+    config 
+}
+
 fn main() -> anyhow::Result<()>{
-    let log_file = std::fs::File::create("log.txt").expect("Could not create the log file");
+    let config = get_config();
+    let log_file = std::fs::File::create(config.config_dir.join("logs.txt")).expect("Could not create the log file");
     WriteLogger::init(LevelFilter::max(), Config::default(), log_file).unwrap();
+    
 
     info!("Application started");
 
